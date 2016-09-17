b7210d6abbb97a02dec29db6770cb1492b015ce1
Tejasvi (Teju) Nareddy <tejunareddy@gmail.com>
2016-09-17 19:32:48 -0400

    Change script to call different Nasdaq API

--- Assets/Scripts/Test.cs
+++ Assets/Scripts/Test.cs
@@ -8,15 +8,13 @@ public class Test : MonoBehaviour
 
 	public void Start()
     {
-        string url = "http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetSummarizedTrades";
+        string url = "http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetEndOfDayData";
         WWWForm form = new WWWForm();
         form.AddField("_Token", "247F80E1279F451499B6D68857FA0A93");
-        form.AddField("Symbols", "AAPL,MSFT");
-        form.AddField("StartDateTime", "2/1/2015 00:00:00.000");
-        form.AddField("EndDateTime", "2/18/2015 23:59:59.999");
+        form.AddField("Symbols", "AAPL,MSFT,GOOG");
+        form.AddField("StartDate", "9/16/2016");
+        form.AddField("EndDate", "9/16/2016");
         form.AddField("MarketCenters", "");
-        form.AddField("TradePrecision", "Hour");
-        form.AddField("TradePeriod", "1");
         WWW www = new WWW(url, form);
         StartCoroutine(WaitForRequest(www));
     }
