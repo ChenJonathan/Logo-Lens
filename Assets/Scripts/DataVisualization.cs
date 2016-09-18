76706dffd2ee2c87b2058588ea43f47422fb5350
Jonathan Chen <jonathanchen@gatech.edu>
2016-09-18 01:51:05 -0400

    Added basic card display

---
+++ Assets/Scripts/DataVisualization.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class DataVisualization : MonoBehaviour
+{
+    [SerializeField]
+    TextMesh signPrefab;
+
+    public void DisplayCard(string ticker, double start, double end)
+    {
+        TextMesh temp = (TextMesh)Instantiate(signPrefab, transform.position + transform.forward * 10, Quaternion.identity);
+        temp.text = ticker + ": $" + start.ToString("F2") + ", $" + end.ToString("F2");
+    }
+}
--- Assets/Scripts/GetStockData.cs
+++ Assets/Scripts/GetStockData.cs
@@ -3,14 +3,14 @@ using System.Collections;
 using System.Xml;
 using System.Collections.Generic;
 
-public class Nasdaq : MonoBehaviour {
+public class GetStockData : MonoBehaviour {
 
-    public void callNasdaqAPI(string startDate, string endDate, string tickers)
+    public void callNasdaqAPI(string startDate, string endDate, string ticker)
     {
         string url = "http://ws.nasdaqdod.com/v1/NASDAQAnalytics.asmx/GetEndOfDayData";
         WWWForm form = new WWWForm();
         form.AddField("_Token", "247F80E1279F451499B6D68857FA0A93");
-        form.AddField("Symbols", tickers);
+        form.AddField("Symbols", ticker);
         form.AddField("StartDate", startDate);
         form.AddField("EndDate", endDate);
         form.AddField("MarketCenters", "");
@@ -38,6 +38,7 @@ public class Nasdaq : MonoBehaviour {
                 string close = EndOfDayPrice.ChildNodes[5].InnerText;
 
                 Debug.Log(symbol + ": " + open + " to " + close);
+                GetComponent<DataVisualization>().DisplayCard(symbol, float.Parse(open), float.Parse(close));
 
                 if (isFirstTime == 0)
                 {
