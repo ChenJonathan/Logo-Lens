3a3c4213e96fda134489cecdb9e4f6ff4babc10a
ndass6 <ND@USComputers.net>
2016-09-18 05:00:07 -0400

    Change color of text based on change in price

--- Assets/Scripts/DataVisualization.cs
+++ Assets/Scripts/DataVisualization.cs
@@ -9,5 +9,21 @@ public class DataVisualization : MonoBehaviour
     {
         TextMesh temp = (TextMesh) Instantiate(signPrefab, transform.position + transform.forward * 10, Quaternion.identity);
         temp.text = ticker + ": $" + start.ToString("F2") + ", $" + end.ToString("F2");
+
+        // Make the text red if the price went down
+        if (start > end)
+        {
+            temp.color = Color.red;
+        }
+        // Make the text green if the price went up
+        else if (start < end)
+        {
+            temp.color = Color.green;
+        }
+        // Make the text black if the price stayed the same
+        else
+        {
+            temp.color = Color.black;
+        }
     }
 }
