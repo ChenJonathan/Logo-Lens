7233606204b532d73ae4bfa306e68d86fa7dc0b2
Jonathan Chen <jonathanchen@gatech.edu>
2016-09-18 10:02:58 -0400

    Added improved card display

--- Assets/Scripts/DataVisualization.cs
+++ Assets/Scripts/DataVisualization.cs
@@ -3,27 +3,31 @@
 public class DataVisualization : MonoBehaviour
 {
     [SerializeField]
-    TextMesh signPrefab;
+    GameObject signPrefab;
 
     public void DisplayCard(string ticker, double start, double end)
     {
-        TextMesh temp = (TextMesh) Instantiate(signPrefab, transform.position + transform.forward * 10, Quaternion.identity);
-        temp.text = ticker + ": $" + start.ToString("F2") + ", $" + end.ToString("F2");
+        GameObject temp = (GameObject)Instantiate(signPrefab, transform.position + transform.forward * 10, Quaternion.identity);
+        ((TextMesh)temp.transform.FindChild("Ticker").GetComponent<TextMesh>()).text = ticker;
+        ((TextMesh)temp.transform.FindChild("Date").GetComponent<TextMesh>()).text = "9/16/2016";
+        ((TextMesh)temp.transform.FindChild("Open Price").GetComponent<TextMesh>()).text = "$" + start.ToString("F2");
+        ((TextMesh)temp.transform.FindChild("Close Price").GetComponent<TextMesh>()).text = "$" + end.ToString("F2");
+        ((TextMesh)temp.transform.FindChild("Percent Change").GetComponent<TextMesh>()).text = ((end - start) / start).ToString("F3") + "%";
 
         // Make the text red if the price went down
         if (start > end)
         {
-            temp.color = Color.red;
+            ((TextMesh)temp.transform.FindChild("Percent Change").GetComponent<TextMesh>()).color = Color.red;
         }
         // Make the text green if the price went up
         else if (start < end)
         {
-            temp.color = Color.green;
+            ((TextMesh)temp.transform.FindChild("Percent Change").GetComponent<TextMesh>()).color = Color.green;
         }
         // Make the text black if the price stayed the same
         else
         {
-            temp.color = Color.black;
+            ((TextMesh)temp.transform.FindChild("Percent Change").GetComponent<TextMesh>()).color = Color.black;
         }
     }
 }
--- Assets/Scripts/GetTicker.cs
+++ Assets/Scripts/GetTicker.cs
@@ -1,52 +1,59 @@
 using UnityEngine;
 using System.Collections.Generic;
 using System.IO;
-
 public class GetTicker : MonoBehaviour
 {
     private Dictionary<string, string[]> stocks = new Dictionary<string, string[]>();
-
     public void Start()
     {
-        TextAsset reader = Resources.Load("companylist.csv") as TextAsset;
+        TextAsset reader = Resources.Load("companylist") as TextAsset;
         string raw = reader.text;
         string[] lines = raw.Split('\n');
-        for (int i = 0; i < lines.Length; i++)
+        for(int i = 0; i < lines.Length; i++)
         {
             var values = lines[i].Split(',');
-            
-            string[] info = new string[values.Length - 2];
-            info[0] = values[0];
-            for (int j = 2; j < values.Length - 1; j++)
+            if(values.Length - 2 >= 0)
             {
-                info[j - 1] = values[j];
+                string[] info = new string[values.Length - 2];
+                info[0] = values[0];
+                for(int j = 2; j < values.Length - 1; j++)
+                {
+                    info[j - 1] = values[j];
+                }
+                if(!stocks.ContainsKey(values[1].ToLower().Replace(' ', '-')))
+                {
+                    stocks.Add(values[1].ToLower().Replace(' ', '-'), info);
+                }
             }
-            stocks.Add(values[1].ToLower().Replace(' ', '-'), info);
         }
     }
-
     // Does the ticker thingy of the company that is named "name"
-    public string getTicker(string name) {
+    public string getTicker(string name)
+    {
         name = name.ToLower().Replace(" ", "-");
-        foreach (KeyValuePair<string, string[]> entry in stocks) {
-            if (entry.Key.Contains(name)) {
+        foreach(KeyValuePair<string, string[]> entry in stocks)
+        {
+            if(entry.Key.Contains(name))
+            {
                 string ticker = entry.Value[0];
                 ticker = ticker.Replace("\"", "");
                 return ticker;
             }
         }
-
         return "";
     }
-
     // Returns the info of the company that is named "name" where infoPositions
     // contains the positions in the infoList of the company that is desired
-    string[] getInfo(string name, int[] infoPositions) {
+    string[] getInfo(string name, int[] infoPositions)
+    {
         name = name.ToLower().Replace(" ", "-");
-        foreach (KeyValuePair<string, string[]> entry in stocks) {
-            if (entry.Key.Contains(name)) {
+        foreach(KeyValuePair<string, string[]> entry in stocks)
+        {
+            if(entry.Key.Contains(name))
+            {
                 string[] info = new string[infoPositions.Length];
-                for (int j = 0; j < info.Length; j++) {
+                for(int j = 0; j < info.Length; j++)
+                {
                     info[j] = entry.Value[infoPositions[j]];
                 }
             }
